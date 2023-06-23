d5752839e5ad4c0d610c2e858ac216b465941027
Dominik Dadański <ddadanski@edu.cdv.pl>
2023-06-23 19:27:35 +0200

    Object Scripts

---
+++ Assets/Script/PickableObject.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickableObject : MonoBehaviour
+{
+    [SerializeField] private PickableObjectSO pickableObjectSO;
+
+    private IHuntedObjectsParent pickableObjectParent;
+
+    public PickableObjectSO GetPickableObjectSO()
+    {
+        return pickableObjectSO;
+    }
+
+    public void SetPickableObjectParent(IHuntedObjectsParent pickableObjectParent)
+    {
+        if (this.pickableObjectParent != null)
+        {
+            this.pickableObjectParent.ClearPickableObject();
+        }
+        this.pickableObjectParent = pickableObjectParent;
+        if (pickableObjectParent.HasPickableObject())
+        {
+            Debug.LogError("PickableObjectParent posiada ju¿ obiekt!");
+        }
+        pickableObjectParent.SetPickableObject(this);
+        transform.parent = pickableObjectParent.GetPickableObjectFollowTransform();
+        transform.localPosition = Vector3.zero;
+
+    }
+
+    public IHuntedObjectsParent GetKitchenObjectParent()
+    {
+        return pickableObjectParent;
+    }
+
+    public void DestroySelf()
+    {
+        pickableObjectParent.ClearPickableObject();
+        Destroy(gameObject);
+    }
+
+    /*
+    public static PickableObject SpawnPickableObject(PickableObjectSO pickableObjectSO, IHuntedObjectsParent huntedObjectParent)
+    {
+        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
+        KitchenObject kitchenObject = kitchenObjectTransform.GetComponent<KitchenObject>();
+        kitchenObject.SetKitchenObjectParent(kitchenObjectParent);
+        return kitchenObject;
+    }
+    */
+}

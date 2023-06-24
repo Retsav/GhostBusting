939c565719e10022beea6a9323e74911bba55f81
Dominik Dadański <ddadanski@edu.cdv.pl>
2023-06-24 20:09:30 +0200

    Quick Time Event Scriptable Object

---
+++ Assets/Script/QTE_SO.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu()]
+public class QTE_SO : ScriptableObject
+{
+    public KeyCode keyToPress;
+    public Sprite keySprite;
+}
